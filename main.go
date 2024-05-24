e41af13ecdb64a5c3b7d03534e116c1375536379
MarcelArt <badman.islemon@gmail.com>
2024-05-24 13:36:17 +0700

    Initial project setup

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"game_planner_backend/database"
+	"game_planner_backend/router"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	database.ConnectDB()
+
+	router.SetupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
+}
